75926ef5a054d5a053ef233d4c71d2e54af02694
Stefanie Ding <StefanieDing@Stefanie-Dings-MacBook-Pro.local>
2016-08-29 18:19:23 -0400

    got rid of wrong variable

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -62,7 +62,6 @@ var triviaQuestions = [{
 
 var gifArray = ['question1', 'question2', 'question3', 'question4', 'question5', 'question6', 'question7', 'question8', 'question9', 'question10', 'question11', 'question12', 'question13','question14','question15'];
 var currentQuestion; var correctAnswer; var incorrectAnswer; var unanswered; var seconds; var time; var answered; var userSelect;
-var wrong = false;
 var messages = {
 	correct: "Yes, that's right!",
 	incorrect: "No, that's not it.",
